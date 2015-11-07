1586ecc86745c9353e4838f986ede095ffc22ade
Rick Patci <rpatci@gmail.com>
2015-11-06 21:50:48 -0800

    finish array methods

--- pushpopetc.js
+++ pushpopetc.js
@@ -1,23 +1,38 @@
 /* re-implement: Push, Pop, Shift, and Unshift
 for Array.prototype, without using any library functions */
 
-Array.prototype.RickPush = function(arg) {
-  var positions = this.length;
-  this[positions] = arg;
-  return this.length;
+
+module.exports.RickPush = function RickPush(arr) {
+  for (var i = 1; i < arguments.length; i ++) {
+    arr[arr.length] = arguments[i];
+  }
+  return arr.length;
 }
 
-Array.prototype.RickPop = function() {
-  var popped = this[this.length-1];
-  this.length --;
+module.exports.RickPop = function RickPop(arr) {
+  var popped = arr[arr.length-1];
+  arr.length --;
   return popped;
 }
 
-Array.prototype.RickShift = function() {
-  return this.splice(0,1);
+module.exports.RickShift = function RickShift(arr) {
+  var shifted = arr[0];
+  for (var i = 0; i < arr.length-1; ++i) {
+    arr[i] = arr[i+1];
+  }
+  arr.length --;
+  return shifted;
 }
 
-Array.prototype.RickUnshift = function(arg) {
-
-  return this.length;
+module.exports.RickUnShift = function RickUnShift(arr) {
+  // Worked with Jordan Bundy on this part:
+  for (var i = arr.length-1; i >= 0; i--) {
+    arr[(arguments.length-1) + i] = arr[i];
+  }
+  for (i = 1; i < arguments.length; i++) {
+    arr[i-1] = arguments[i];
+  }
+  return arr.length;
 }
+
+
