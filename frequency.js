1f4426f774f23604997e69d1f43a15dbffa2e604
Rick Patci <rpatci@gmail.com>
2015-11-07 17:58:05 -0800

    update frequency.js

--- frequency.js
+++ frequency.js
@@ -1,22 +1,25 @@
 /* given an array of english words, figure out what
-  the most common letter (anywhere in a word) is. */
+  the most common letter is. */
 
-var words = ['Rick', 'Craig'];
-var letters = [];
-
-function getLetters(arr) {
-  letters.push(arr.join('').toLowerCase());
-  letters = letters[0].split('').sort();
-  return letters;
-}
-
-getLetters(words);
-
-function mostCommon(stringArr) {
-  var theMost = '';
-  for (i = 0; i < stringArr.length; i++) {
-
-    if(stringArr[i] )
+module.exports.frequency = function frequency(arr) {
+  var letters = '';
+  // Object use per convo with Jordan Bundy and Tyler Morgan.
+  var alphabet = {};
+  var mostFreq;
+  for(i = 0; i < arr.length; i++) {
+    (letters += arr[i]).toLowerCase();
   }
+  for(i = 0; i < letters.length; i++) {
+    if (alphabet[letters[i].toLowerCase()]) {
+      ++alphabet[letters[i].toLowerCase()];
+    } else alphabet[letters[i].toLowerCase()] = 1;
+    if ((alphabet[mostFreq] || 0) < alphabet[letters[i]]) mostFreq = letters[i];
+  }
+  return mostFreq;
 }
+
+
+
+
+
 
